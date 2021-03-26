0386691aace3bb43a9febe50a0250b2a12c18c1a
ThinPotato <bryce940@gmail.com>
2021-03-26 18:09:52 -0400

    improved comments

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -7,9 +7,6 @@ rawPacket = open('packet.pcap','rb')
 flowDictionary = defaultdict(list)
 pcap = dpkt.pcap.Reader(rawPacket)
 flowCount = 0
-lastPacketBeforeCloseLocation = 0
-packetSize = 0
-numofFails = 0
 estimatedWindow =1
 
 # Convert byte data to numbers
@@ -20,22 +17,17 @@ def inet_to_str(inet):
     except ValueError:
         return socket.inet_ntop(socket.AF_INET6, inet)
 
-# Create a new flow and parse the data
+# Organize flows into a dictionary, sort out useless data
 def new_flow():
     global flowCount
-    global packetsList
-    global ipList
     for i, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            # We are only interested in IP packets
             continue
         ip = eth.data
         if ip.p != dpkt.ip.IP_PROTO_TCP:
-            # We are only interested in TCP
             continue
         tcp = ip.data
-
         # Count flows
         if ((tcp.flags & dpkt.tcp.TH_SYN) and (tcp.flags & dpkt.tcp.TH_ACK)):
             flowCount +=1
@@ -44,7 +36,7 @@ def new_flow():
 
 new_flow()
 
-#Check for 3 way handshakes
+# Parse for data
 for flows in flowDictionary:
     numofFails=0
     numofTimeout = 0
@@ -54,26 +46,28 @@ for flows in flowDictionary:
     for x in range(len(packetsList)):
         # Homework asks for congestion window estimated at SENDER. So we ignore port 80 which is the reciever
         if(packetsList[x].data.sport != 80):
-            # ~~~Estimate window size~~~
+        # ~~~Estimate window size~~~
             # If packet is ack increase estimated window by 1
             if(packetsList[x].data.flags & dpkt.tcp.TH_ACK):
                 estimatedWindow +=1
 
-        #~~Find number of times retransmission occured~~
+    #~~Find number of times retransmission occured~~~
         # Due to tripple ack
         subPacketsList = packetsList[x:x+3]
         if all(v.data.ack == subPacketsList[0].data.ack for v in subPacketsList):
             if all(u.data.seq == subPacketsList[0].data.seq for u in subPacketsList):
                 numofFails += 1
+            # ~~~Estimate window size~~~
                 if(packetsList[x].data.sport != 80):
                     estimatedWindow //=2
         # Due to timeout
         if(packetsList[x-1].data.seq > packetsList[x].data.seq):
             numofTimeout += 1
+        # ~~~Estimate window size~~~
             if(packetsList[x].data.sport != 80):
                 estimatedWindow //=2
         lastSeq = packetsList[x].data.seq
-            # Print Basic Data 
+            # Print Basic Data, part A
                 # part (a)
         if (x ==1):
             print("Source Port: ", packetsList[x+1].data.sport)
@@ -96,12 +90,14 @@ for flows in flowDictionary:
         packetSize += len(packetsList[x].data.data)
         
         window += dpkt.tcp.TCP_OPT_WSCALE
+    #~~~ Part A (c) ~~~
     if(packetsList[x].data.sport != 80):
         print("throughput: ",packetSize, "bytes")
+    #~~~Part B(1)~~~
     print("Calculated Average window size: ", (window * 16385)/len(packetsList))
-        
+    #~~~ Part B (2)~~~
     print("\nNumber of retramissions due to triple ACK duplicate:", numofFails)
     print("Number of retramissions due to timeout:", numofTimeout)
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-# Print total number of flows
+# Print total number of flows Part A
 print(flowCount, "flows detected")
